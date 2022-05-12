e854627e8e4cdcea54dac76a9840cd0bdf5bef38
Carlos Raez <carlosraez@gmail.com>
2022-05-12 15:39:55 +0200

    add Provider at App.tsx

--- App.tsx
+++ App.tsx
@@ -3,10 +3,18 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { Navigator } from './src/router/navigator';
 
+import { PermissionsProvider } from './src/context/permissionscontext';
+
+const AppState = ({ children }: any) => {
+  return <PermissionsProvider>{children}</PermissionsProvider>;
+};
+
 const App = () => {
   return (
     <NavigationContainer>
-      <Navigator />
+      <AppState>
+        <Navigator />
+      </AppState>
     </NavigationContainer>
   );
 };
